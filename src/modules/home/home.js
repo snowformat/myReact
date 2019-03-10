041fc6cad9f73c294798ff871811566e0bf7db7f
snow <472171252@qq.com>
2019-03-11 02:20:10 +0800

    完成轮播图效效果

--- src/modules/home/home.js
+++ src/modules/home/home.js
@@ -1,17 +1,48 @@
 import React from "react";
+import {Input} from "semantic-ui-react"
+// import "semantic-ui-css/semantic.min.css"
+import ImageGallery from 'react-image-gallery';
+import axios from "axios";
+import "react-image-gallery/styles/css/image-gallery.css";
+import "./home.css";
 
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+           list: []
         }
     }
+    componentDidMount() {
+        axios.post("homes/swipe").then(data => {
+            if(data.meta.status === 200) {
+                this.setState({
+                    list: data.data.list
+                })
+            }
+        })
+    }
     render () {
+        // const images = [
+        //     {
+        //       original: 'http://lorempixel.com/1000/600/nature/1/'
+        //     },
+        //     {
+        //       original: 'http://lorempixel.com/1000/600/nature/2/'
+        //     },
+        //     {
+        //       original: 'http://lorempixel.com/1000/600/nature/3/'
+        //     }
+        //   ]
         return (
-            <div>
-                主页
+            <div className="main-content">
+                <div className="main-topbar">
+                    <Input icon={{ name: 'search', circular: true, link: true }} placeholder='Search...' className="search-input" />
+                </div>
+                <div>
+                    <ImageGallery items={this.state.list} showThumbnails={false}/> 
+                </div>
             </div>
         )
     }
